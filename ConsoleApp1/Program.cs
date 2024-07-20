2c13157320e23f3ab621268e84ebaa46aaeac989
Yach <135635030+y1ach@users.noreply.github.com>
2024-07-20 15:38:59 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿string username = "admin";
+string password = "123";
+Console.WriteLine("Kullanıcı Adı:");
+string userusername = Console.ReadLine();
+Console.WriteLine("Şifre:");
+string userpassword = Console.ReadLine();
+bool enter = username == userusername && password == userpassword;
+Console.WriteLine("Giriş Durumu:" + enter);
